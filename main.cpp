c7b8cd32ee17e3bb6eeb9277a07ad04f6c9de8f6
Scott <scott.heidbrink@gmail.com>
2015-05-15 19:12:22 -0600

    Remove vim crap accidentally pushed and structered main code

--- main.cpp
+++ main.cpp
@@ -9,14 +9,23 @@ int main(int argc, char* argv[])
 	UserInterface::Message("Welcome to Dungeons and Commas!");
 	// Sign in/Create Player
 	std::string character = UserInterface::Prompt("What is your username?");
+	Player* character;
 	//Check if Character Exists
+	if(character == NULL)
+	{
 		//Otherwise Create new Player
-		Player* newChar = new Player(character);
-		UserInterface::Message("Created New Player: " + newChar->name);
+		character = new Player(character);
+		UserInterface::Message("Created New Player: " + character->name);
+		UserInterface::Message("You randomly appear in a strange new world");
+		// Randomize where they are put? that could be fun
 		Room* testRoom = new Room("This is a room", "A test room");
 		UserInterface::Message(testRoom->EnterCreature(newChar));
-	// Prompt of them waking up
-	std::string command = UserInterface::Prompt("Well that was weird passing out again");
+	}
+	else
+	{
+		// Prompt of them waking up
+		std::string command = UserInterface::Prompt("Well that was weird passing out again");
+	}
 	while(command.compare("sleep") != 0)
 	{
 		command = UserInterface::Prompt("");
